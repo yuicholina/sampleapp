f9ce60989490e0904793266557a788571f45d159
Yui Murase <yuicholina.m@gmail.com>
2014-08-11 15:07:47 +0900

    Initialize repository

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  let(:base_title){"Sample"}
+
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the title 'Sample'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title}")
+    end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+
+  end
+
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit 'static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+   
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
+end
