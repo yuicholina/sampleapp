839421813700ecf22ac0b88e436ddbd6a588ca6f
Yui Murase <yuicholina.m@gmail.com>
2014-08-26 13:11:46 +0900

    11.1.4 11.10

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,9 @@ describe User do
   it{ should respond_to(:feed) }
   it{ should respond_to(:relationships) }
 
+  it{ should respond_to(:relationships) }
+  it{ should respond_to(:followed_users) }
+
 
 
   describe "with admin attribute set to 'true'" do
